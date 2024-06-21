801c5b82520aa37d150cbc43ea20158780846488
Daniel Galvão <galvaoazevedod@gmail.com>
2024-06-21 07:16:31 -0300

    Criação das classes de Transacao:
    - TransacaoController
    - TransacaoService
    - ITransacaoService
    - TransacaoModel

---
+++ myfinance-web-netcore/Controllers/TransacaoController.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using myfinance_web_netcore.Models;
+using myfinance_web_netcore.Infrastructure;
+using AutoMapper;
+using myfinance_web_netcore.Services;
+
+namespace myfinance_web_netcore.Controllers;
+
+[Route("[controller]")]
+public class TransacaoController : Controller
+{
+    private readonly ILogger<TransacaoController> _logger;
+    private readonly ITransacaoService _transacaoService;
+
+    public TransacaoController(
+        ILogger<TransacaoController> logger,
+        ITransacaoService transacaoService)
+    {
+        _logger = logger;
+        _transacaoService = transacaoService;
+    }
+
+    public IActionResult Index()
+    {
+        var lista = _transacaoService.ListarRegistros();
+        ViewBag.ListaPlanoConta = lista;
+        return View();
+    }
+
+    [HttpPost]
+    [Route("Cadastrar")]
+    [Route("Cadastrar/{id}")]
+    public IActionResult Cadastrar(TransacaoModel model, int? id) 
+    {
+        if (ModelState.IsValid) 
+        {
+            _transacaoService.Salvar(model);
+        }
+            
+        return View();
+    }
+
+    [HttpGet]
+    [Route("Cadastrar")]
+    [Route("Cadastrar/{id}")]
+    public IActionResult Cadastrar(int? id) 
+    {
+        if (id != null)
+        {
+            var registro = _transacaoService.RetornarRegistro((int)id);
+            return View(registro);
+        } 
+        return View();
+    }
+
+    [HttpGet]
+    [Route("Excluir/{id}")]
+    public IActionResult Excluir(int id)
+    {
+        _transacaoService.Excluir(id);
+        return RedirectToAction("Index");
+    }
+
+}
---
+++ myfinance-web-netcore/Services/TransacaoService.cs
@@ -0,0 +1,63 @@
+using AutoMapper;
+using myfinance_web_netcore.Domain;
+using myfinance_web_netcore.Infrastructure;
+using myfinance_web_netcore.Models;
+
+namespace myfinance_web_netcore.Services
+{
+     public class TransacaoService : ITransacaoService
+    {
+
+        private readonly IMapper _mapper;
+
+        private readonly MyFinanceDbContext _myFinanceDbContext;
+
+        public TransacaoService(
+            MyFinanceDbContext myFinanceDbContext,
+            IMapper mapper)
+        {
+            _myFinanceDbContext = myFinanceDbContext;
+            _mapper = mapper;
+        }
+
+        public void Excluir(int id)
+        {
+            var item = _myFinanceDbContext.Transacao.Where(x => x.Id == id).First();
+            _myFinanceDbContext.Attach(item);
+            _myFinanceDbContext.Remove(item);
+            _myFinanceDbContext.SaveChanges();
+        }
+
+        public List<TransacaoModel> ListarRegistros()
+        {
+            var listaTransacao = _myFinanceDbContext.Transacao.ToList();
+            var lista = _mapper.Map<List<TransacaoModel>>(listaTransacao);
+            return lista;
+        }
+
+        public TransacaoModel RetornarRegistro(int id)
+        {
+            var item = _myFinanceDbContext.Transacao.Where(x => x.Id == id).First();
+            var registro = _mapper.Map<TransacaoModel>(item);
+            return registro;
+        }
+
+        public void Salvar(TransacaoModel model)
+        {
+            var dbSet = _myFinanceDbContext.Transacao;
+            var item = _mapper.Map<Transacao>(model);
+
+            if (item.Id == 0)
+            {
+                dbSet.Add(item);
+            }
+            else
+            {
+                dbSet.Attach(item);
+                _myFinanceDbContext.Entry(item).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            }
+
+            _myFinanceDbContext.SaveChanges();
+        }
+    }
+}
